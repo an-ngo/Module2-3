9c4b32bd2992a16c6d55369d4ae10fe5750867b2
an.ngo <phowflowf135@gmail.com>
2021-09-17 15:44:54 +0700

    m

---
+++ src/sumCol.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sumCol {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Width and Length of arr");
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int [][]arr=new int[w][h];
+        for(int i=0;i<w;i++){
+            for(int j=0;j<h;j++){
+                System.out.println("Input arr["+i+"]["+j+"]: ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("What column u want to count sum");
+        int row = sc.nextInt();
+        int sum=0;
+        for(int i=0;i<w;i++){
+            sum+=arr[i][row];
+        }
+        System.out.println("Sum of column "+row+" is: "+sum);
+        sc.close();
+    }
+}
